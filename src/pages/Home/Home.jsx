bf1643328931a05c348fa6c936f9a6d5dc23d00d
YannGUIOT <guiot_yann@hotmail.fr>
2023-08-07 14:22:55 +0200

    Visit Card

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,3 @@
-// import { useState } from "react";
-// import { useNavigate } from 'react-router-dom'
-// import { Link } from 'react-router-dom';
-import { useSetAtom} from 'jotai'
-import { nbreArticles } from "../../atoms/atoms";
 
 export const Home = () => {
 

3e3f05337fd18e70ef8a8acd15d0e9257da83c74
YannGUIOT <guiot_yann@hotmail.fr>
2023-08-08 12:34:37 +0200

    Visit Card

--- src/components/CardBoard/CardBoard.jsx
+++ src/components/CardBoard/CardBoard.jsx
@@ -9,6 +9,7 @@ export const CardBoard = () => {
       <div className='grid-item colorsSelection'>colorsSelection</div>
       <div className='grid-item fontsSelection'>fontsSelection</div>
       <div className='grid-item cardDisplay'>cardDisplay</div>
+      <div className='grid-item other'>other</div>
     </div>
   )
 }

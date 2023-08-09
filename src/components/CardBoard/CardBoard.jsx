54247e8d835ba54ff4ca859770f78fd0b849fbcf
YannGUIOT <guiot_yann@hotmail.fr>
2023-08-09 11:41:02 +0200

    Visit Card

--- src/components/CardBoard/CardBoard.jsx
+++ src/components/CardBoard/CardBoard.jsx
@@ -4,11 +4,11 @@ export const CardBoard = () => {
 
   return (
     <div className="grid-container cardBoard ">
+      <div className='grid-item cardDisplay'>cardDisplay</div>
       <div className='grid-item modelSelection'>modelSelection</div>
       <div className='grid-item textsForm'>textsForm</div>
       <div className='grid-item colorsSelection'>colorsSelection</div>
       <div className='grid-item fontsSelection'>fontsSelection</div>
-      <div className='grid-item cardDisplay'>cardDisplay</div>
       <div className='grid-item other'>other</div>
     </div>
   )

3d49650b7cfc274c531a4ccf902b757d142311cd
YannGUIOT <guiot_yann@hotmail.fr>
2023-08-09 22:03:12 +0200

    Visit Card

--- src/components/CardBoard/CardBoard.jsx
+++ src/components/CardBoard/CardBoard.jsx
@@ -3,26 +3,6 @@ import { Card } from '../../class/Card';
 
 import './CardBoard.scss'
 
-// export class CardBoard extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.card = new Card();
-//   }
-
-//   render() {
-//     return (
-//       <div className="grid-container cardBoard">
-//         <div className='grid-item cardDisplay'>{this.card.renderCard()}</div>
-//         <div className='grid-item modelSelection'></div>
-//         <div className='grid-item textsForm'>{this.card.renderEditText()}</div>
-//         <div className='grid-item colorsSelection'>{this.card.renderColorSelect()}</div>
-//         <div className='grid-item fontsSelection'></div>
-//         <div className='grid-item other'></div>
-//       </div>
-//     );
-//   }
-// }
-
 export const CardBoard = () => {
 
 const card = new Card();

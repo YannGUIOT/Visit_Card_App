9e059dff80a3d90aed944a063b555374e389e3b2
YannGUIOT <guiot_yann@hotmail.fr>
2023-08-14 17:33:15 +0200

    Visit Card

--- src/components/CardBoard/CardBoard.jsx
+++ src/components/CardBoard/CardBoard.jsx
@@ -1,5 +1,6 @@
 import { ModelSelection } from '../ModelSelection/ModelSelection';
 import { GlobalSettings } from '../GlobalSettings/GlobalSettings';
+import { TextSettings } from '../TextSettings/TextSettings';
 import { Card } from '../../class/Card';
 import html2canvas from 'html2canvas';
 import { useState } from 'react';
@@ -61,13 +62,6 @@ export const CardBoard = () => {
     setCard({ ...card });
   };
 
-  const changeSize = (field, n) => {
-    if( ((n==-1)&&(card.state[field] > 10)) || ((n==1)&&(card.state[field] < 36)) ) {
-      card.updateCard(field, card.state[field] + n);
-      setCard({ ...card });
-    }
-  };
-  
   const updateCheckbox = (field) => {
     card.updateCard(field, !card.state[field]);
     setCard({ ...card });
@@ -110,227 +104,42 @@ export const CardBoard = () => {
         globalColorChange={updateGlobal}
         resetCard={resetCard}
       />
-      <div className='settings'>
-        <div className='info-row'>
-          <div className='info-col-1'>
-            Title 1 :
-          </div>
-          <div className='info-col-2'>
-            <input
-              type="text"
-              name="title1"
-              value={card.state.title1}
-              onChange={updateChange('title1')}
-              maxLength={24}
-            />
-          </div>
-          <div className='info-col-3'>
-            <input type="color" value={card.state.title1_color} onChange={updateChange('title1_color')} />
-          </div>
-          <div className='info-col-4'>
-            <select value={card.state.title1_font} onChange={updateChange('title1_font')}>
-              {fonts.map((font, index) => (
-                <option key={index} value={font} style={{fontFamily: font}}>
-                  {font}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className='info-col-5'>
-            <button onClick={() => changeSize('title1_size', -1)}>-</button>
-            <button onClick={() => changeSize('title1_size', 1)}>+</button>
-          </div>
-          <div className='info-col-6'>
-            <input
-              type="checkbox"
-              checked={card.state.title1_strong}
-              onChange={() => updateCheckbox('title1_strong')}
-            />
-          </div>
-          <div className='info-col-7'>
-            <input
-              type="checkbox"
-              checked={card.state.title1_border}
-              onChange={() => updateCheckbox('title1_border')}
-            />
-          </div>
-        </div>
-        <div className='info-row'>
-          <div className='info-col-1'>
-            Title 2 :
-          </div>
-          <div className='info-col-2'>
-            <input
-              type="text"
-              name="title2"
-              value={card.state.title2}
-              onChange={updateChange('title2')}
-              maxLength={28}
-            />
-          </div>
-          <div className='info-col-3'>
-            <input type="color" value={card.state.title2_color} onChange={updateChange('title2_color')} />
-          </div>
-          <div className='info-col-4'>
-            <select value={card.state.title2_font} onChange={updateChange('title2_font')}>
-              {fonts.map((font, index) => (
-                <option key={index} value={font} style={{fontFamily: font}}>
-                  {font}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className='info-col-5'>
-            <button onClick={() => changeSize('title2_size', -1)}>-</button>
-            <button onClick={() => changeSize('title2_size', 1)}>+</button>
-          </div>
-          <div className='info-col-6'>
-            <input
-              type="checkbox"
-              checked={card.state.title2_strong}
-              onChange={() => updateCheckbox('title2_strong')}
-            />
-          </div>
-          <div className='info-col-6'>
-            <input
-              type="checkbox"
-              checked={card.state.title2_border}
-              onChange={() => updateCheckbox('title2_border')}
-            />
-          </div>
-        </div>
-        <div className='info-row'>
-          <div className='info-col-1'>
-            Title 3 :
-          </div>
-          <div className='info-col-2'>
-            <input
-              type="text"
-              name="title3"
-              value={card.state.title3}
-              onChange={updateChange('title3')}
-              maxLength={28}
-            />
-          </div>
-          <div className='info-col-3'>
-            <input type="color" value={card.state.title3_color} onChange={updateChange('title3_color')} />
-          </div>
-          <div className='info-col-4'>
-            <select value={card.state.title3_font} onChange={updateChange('title3_font')}>
-              {fonts.map((font, index) => (
-                <option key={index} value={font} style={{fontFamily: font}}>
-                  {font}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className='info-col-5'>
-            <button onClick={() => changeSize('title3_size', -1)}>-</button>
-            <button onClick={() => changeSize('title3_size', 1)}>+</button>
-          </div>
-          <div className='info-col-6'>
-            <input
-              type="checkbox"
-              checked={card.state.title3_strong}
-              onChange={() => updateCheckbox('title3_strong')}
-            />
-          </div>
-          <div className='info-col-6'>
-            <input
-              type="checkbox"
-              checked={card.state.title3_border}
-              onChange={() => updateCheckbox('title3_border')}
-            />
-          </div>
-        </div>
-        <br />
-        <div className='info-row'>
-          <div className='info-col-1'>
-            Email :
-          </div>
-          <div className='info-col-2'>
-            <input
-              type="text"
-              name="email"
-              value={card.state.email}
-              onChange={updateChange('email')}
-            />
-          </div>
-          <div className='info-col-3'>
-            <input type="color" value={card.state.email_color} onChange={updateChange('email_color')} />
-          </div>
-          <div className='info-col-4'>
-            <select value={card.state.email_font} onChange={updateChange('email_font')}>
-              {fonts.map((font, index) => (
-                <option key={index} value={font} style={{fontFamily: font}}>
-                  {font}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className='info-col-5'>
-            <button onClick={() => changeSize('email_size', -1)}>-</button>
-            <button onClick={() => changeSize('email_size', 1)}>+</button>
-          </div>
-          <div className='info-col-6'>
-            <input
-              type="checkbox"
-              checked={card.state.email_strong}
-              onChange={() => updateCheckbox('email_strong')}
-            />
-          </div>
-          <div className='info-col-6'>
-            <input
-              type="checkbox"
-              checked={card.state.email_border}
-              onChange={() => updateCheckbox('email_border')}
-            />
-          </div>
-        </div>
-        <div className='info-row'>
-          <div className='info-col-1'>
-            Phone :
-          </div>
-          <div className='info-col-2'>
-            <input
-              type="text"
-              name="phone"
-              value={card.state.phone}
-              onChange={updateChange('phone')}
-              maxLength={18}
-            />
-          </div>
-          <div className='info-col-3'>
-            <input type="color" value={card.state.phone_color} onChange={updateChange('phone_color')} />
-          </div>
-          <div className='info-col-4'>
-            <select value={card.state.phone_font} onChange={updateChange('phone_font')}>
-              {fonts.map((font, index) => (
-                <option key={index} value={font} style={{fontFamily: font}}>
-                  {font}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className='info-col-5'>
-            <button onClick={() => changeSize('phone_size', -1)}>-</button>
-            <button onClick={() => changeSize('phone_size', 1)}>+</button>
-          </div>
-          <div className='info-col-6'>
-            <input
-              type="checkbox"
-              checked={card.state.phone_strong}
-              onChange={() => updateCheckbox('phone_strong')}
-            />
-          </div>
-          <div className='info-col-6'>
-            <input
-              type="checkbox"
-              checked={card.state.phone_border}
-              onChange={() => updateCheckbox('phone_border')}
-            />
-          </div>
-        </div>
+      <div className='textSettings'>
+        <TextSettings
+          field="title1"
+          title='Title 1'
+          textSettings={card.state}
+          updateChanges={updateChanges}
+          updateCheckbox={updateCheckbox}
+        />
+        <TextSettings
+          field="title2"
+          title='Title 2'
+          textSettings={card.state}
+          updateChanges={updateChanges}
+          updateCheckbox={updateCheckbox}
+        />
+        <TextSettings
+          field="title3"
+          title='Title 3'
+          textSettings={card.state}
+          updateChanges={updateChanges}
+          updateCheckbox={updateCheckbox}
+        />
+        <TextSettings
+          field="email"
+          title='Email'
+          textSettings={card.state}
+          updateChanges={updateChanges}
+          updateCheckbox={updateCheckbox}
+        />
+        <TextSettings
+          field="phone"
+          title='Phone'
+          textSettings={card.state}
+          updateChanges={updateChanges}
+          updateCheckbox={updateCheckbox}
+        />
       </div>
       <div className='cardDisplay'>{card.renderCard()}</div>
       <div className='download'>
---
+++ src/components/TextSettings/TextSettings.jsx
@@ -0,0 +1,100 @@
+import React from 'react';
+
+  const fonts = [
+    "Arial",
+    "Courier New",
+    "Times New Roman",
+    "Trebuchet MS",
+    "Roboto",
+    "Kanit",
+    "Dancing Script",
+    'Caprasimo',
+    'Caveat',
+    'Indie Flower',
+    'Lilita One', 
+    'Pacifico',
+    'Permanent Marker', 
+    'Tektur',
+    'Ysabeau SC',
+    'Princess Sofia',
+    'Audiowide',
+    'Concert One',
+    'Montez',
+    'Cinzel',
+    'Dekko'
+  ];
+
+export class TextSettings extends React.Component {
+
+  handleChangeValue = (field, event) => {
+    const newValue = event.target.value;
+    this.props.updateChanges(field, newValue);
+  };
+
+  handleSizeChange = (field, delta) => {
+    const newSize = this.props.textSettings[field] + delta;
+    if (newSize >= 10 && newSize <= 36) {
+      this.props.updateChanges(field, newSize);
+    }
+  };
+
+  handleCheckboxChange = (field) => {
+    this.props.updateCheckbox(field);
+  };
+
+  render() {
+    const { field, textSettings, title } = this.props;
+
+    return (
+      <div className="info-row">
+        <div className="info-col-1">{title} :</div>
+        <div className="info-col-2">
+          <input
+            type="text"
+            name={field}
+            value={textSettings[field]}
+            onChange={(event) => this.handleChangeValue(field, event)}
+            maxLength={field === 'email' ? 50 : 28}
+          />
+        </div>
+        <div className="info-col-3">
+          <input
+            type="color"
+            value={textSettings[`${field}_color`]}
+            onChange={(event) => this.handleChangeValue(`${field}_color`, event)}
+          />
+        </div>
+        <div className="info-col-4">
+          <select
+            value={textSettings[`${field}_font`]}
+            onChange={(event) => this.handleChangeValue(`${field}_font`, event)}
+          >
+            {fonts.map((font, index) => (
+                <option key={index} value={font} style={{fontFamily: font}}>
+                  {font}
+                </option>
+              ))}
+          </select>
+        </div>
+        <div className="info-col-5">
+          <button onClick={() => this.handleSizeChange(`${field}_size`, -1)}>-</button>
+          <button onClick={() => this.handleSizeChange(`${field}_size`, 1)}>+</button>
+        </div>
+        <div className="info-col-6">
+          <input
+            type="checkbox"
+            checked={textSettings[`${field}_strong`]}
+            onChange={() => this.handleCheckboxChange(`${field}_strong`)}
+          />
+        </div>
+        <div className="info-col-6">
+          <input
+            type="checkbox"
+            checked={textSettings[`${field}_border`]}
+            onChange={() => this.handleCheckboxChange(`${field}_border`)}
+          />
+        </div>
+      </div>
+    );
+  }
+}
